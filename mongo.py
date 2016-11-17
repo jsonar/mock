57ded7568607f002fa1eac01af94f720d726e4c7
Yuval Hager <yhager@yhager.com>
2016-11-17 15:57:31 -0800

    reject => delayed, conciser logs, pep8

--- mongo.py
+++ mongo.py
@@ -154,6 +154,6 @@ class MockGidCollection(MockCollection):
             {'_id': 3, 'gid': 3, 'gmachine_id': '3', 'hostname': 'host3',
              'version': 10, 'current': True},
             {'_id': 4, 'gid': 4, 'gmachine_id': '3', 'current': False},
-            {'_id': 5, 'gid': 5, 'gmachine_id': '5', 'hostname': 'host5','version': 10,
-             'current': True}
+            {'_id': 5, 'gid': 5, 'gmachine_id': '5', 'hostname': 'host5',
+             'version': 10, 'current': True}
         ])
