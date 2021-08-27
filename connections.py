6b05590450e0f88ca457a9f0f87f2e8b37d45ad9
Maor Feldinger <maor.feldinger@imperva.com>
2021-08-27 09:59:17 -0700

    Add delete_endpoints

--- connections.py
+++ connections.py
@@ -1,6 +1,5 @@
 import json
 
-
 crypto_url = 'https://localhost:27999/crypto/current-key'
 connection_url = 'https://localhost:27902/jsonar/SonarConnections/1.0.0'
 assets_url = connection_url + '/assets'
@@ -11,15 +10,15 @@ class MockAssetsServer:
     def __init__(self, data):
         self.db = MockedDB(data)
 
-    def requests(self, method, url, **kwargs):
+    def request(self, method, url, **kwargs):
         if method == 'GET':
             return self.get_endpoints(url, **kwargs)
         elif method == 'PUT':
             return self.put_endpoints(url, **kwargs)
         elif method == 'POST':
-            pass
+            return self.post_endpoints(url, **kwargs)
         elif method == 'DELETE':
-            pass
+            return self.delete_endpoints(url, **kwargs)
 
     def get_endpoints(self, url, **kwargs):
         if url == crypto_url:
@@ -29,7 +28,10 @@ class MockAssetsServer:
             return self.get_asset(asset_id)
         elif connections_url in url:
             asset_id = kwargs['fields']['asset_id']
-            return self.get_connection(asset_id)
+            return self.get_connections(asset_id)
+        elif connections_url in url:
+            pass
+
         return MockResponse('Page not found', 404, 'NOT FOUND')
 
     def put_endpoints(self, url, **kwargs):
@@ -39,42 +41,91 @@ class MockAssetsServer:
         elif connections_url in url:
             connection = json.loads(kwargs['body'])
             return self.set_connection(connection)
+
+        return MockResponse('Page not found', 404, 'NOT FOUND')
+
+    def post_endpoints(self, url, **kwargs):
+        if assets_url in url:
+            asset_id = url.rsplit('/', 1)[1]
+            asset = json.loads(kwargs['body'])
+            return self.update_asset(asset_id, asset)
+        elif connections_url in url:
+            _id = url.rsplit('/', 1)[1]
+            connection = json.loads(kwargs['body'])
+            return self.update_connection(_id, connection)
+
+        return MockResponse('Page not found', 404, 'NOT FOUND')
+
+    def delete_endpoints(self, url, **kwargs):
+        if assets_url in url:
+            pass
+        elif connections_url in url:
+            _id = url.rsplit('/', 1)[1]
+            return self.delete_connection(_id)
+
         return MockResponse('Page not found', 404, 'NOT FOUND')
 
     def get_asset(self, asset_id):
         asset = self.db.get_asset(asset_id)
-        if asset != 'NOT FOUND':
+        if asset is not None:
             return MockResponse(json.dumps(asset), 200, 'OK')
 
         return MockResponse(f'Failed to get asset with asset_id: {asset_id} ',
                             404, 'NOT FOUND')
 
-    def get_connection(self, asset_id):
-        asset = self.get_asset(asset_id)
-        if asset.reason == 'OK':
-            connection = self.db.get_connection(asset_id)
-            if (connection == 'NOT FOUND' or
-                    connection['asset_id'] == 'missing_mandatory_fields'):
-                return MockResponse('No asset or missing mandatory fields',
-                                    500, 'INTERNAL SERVER ERROR')
-            return MockResponse(json.dumps([connection]), 200, 'OK')
-        return MockResponse(json.dumps('NOT FOUND'), 200, 'OK')
+    def get_connections(self, asset_id):
+        connections = self.db.get_connections(asset_id)
+        if connections:
+            return MockResponse(json.dumps(connections), 200, 'OK')
+
+        return MockResponse(f'Failed to get connections with asset_id: {asset_id} ',
+                            404, 'NOT FOUND')
 
     def set_asset(self, asset):
         if asset and asset['asset_id'] != 'missing_mandatory_fields':
             if self.db.set_asset(asset):
-                return MockResponse(json.dumps({'url': f'{assets_url}/asset/{asset["asset_id"]}'}),
-                                    201, 'CREATED')
-            return MockResponse(json.dumps(f'An asset with id: {asset["asset_id"]} already exists'),
-                                409, 'CONFLICT')
+                return MockResponse(
+                    json.dumps({'url': f'{assets_url}/asset/{asset["asset_id"]}'}),
+                    201, 'CREATED')
+            else:
+                return MockResponse('Asset exists', 409, 'CONFLICT')
         else:
-            return MockResponse(json.dumps('Bad Request'), 400, 'BAD REQUEST')
+            return MockResponse('Missing mandatory fields', 400,
+                                'BAD REQUEST')
 
     def set_connection(self, connection):
-        if self.db.set_connection(connection):
-            return MockResponse(json.dumps({'url': f'{connections_url}/{connection["asset_id"]}'}),
-                                201, 'CREATED')
-        return MockResponse(json.dumps('Bad Request'), 400, 'BAD REQUEST')
+        response = self.db.set_connection(connection)
+        if response['success']:
+            return MockResponse(
+                json.dumps(
+                    {'url': f'{connections_url}/{connection["asset_id"]}'}
+                ), 201, 'CREATED')
+        else:
+            return MockResponse(response['message'], 400,
+                                'BAD REQUEST')
+
+    def update_asset(self, asset_id, asset):
+        if self.db.update_asset(asset_id, asset):
+            return MockResponse(json.dumps(asset), 200, 'OK')
+
+        return MockResponse(f'No asset with asset_id: {asset_id} ',
+                            404, 'NOT FOUND')
+
+    def update_connection(self, _id, connection):
+        if self.db.update_connection(_id, connection):
+            return MockResponse(json.dumps(connection), 200, 'OK')
+
+        return MockResponse(f'No conection with asset_id: {_id} ',
+                            404, 'NOT FOUND')
+
+    def delete_connection(self, _id):
+        for connection in self.db.connection:
+            if connection['_id'] == _id:
+                self.db.connection.remove(connection)
+                return MockResponse(json.dumps(connection), 200, 'OK')
+
+        return MockResponse(f'No resource found to update: {_id} ',
+                            500, 'NOT FOUND')
 
     @property
     def get_current_crypto(self):
@@ -91,31 +142,58 @@ class MockResponse:
 class MockedDB:
     def __init__(self, data):
         self.name = 'lmrm__sonarg'
-        self.asset = data['assets']
-        self.connection = data['connections']
+        self.asset = data['assets'].copy()
+        self.connection = data['connections'].copy()
 
     def set_asset(self, asset):
-        success = False
-        if asset and self.get_asset(asset['asset_id']) == 'NOT FOUND':
+        if not self.get_asset(asset['asset_id']):
             self.asset.append(asset)
-            success = True
-        return success
+            return True
+        else:
+            return False
 
     def set_connection(self, connection):
-        if connection and connection['asset_id'] != 'missing_mandatory_fields':
+        if connection['asset_id'] == 'missing_mandatory_fields':
+            response = {'success': False, 'message': 'missing_mandatory_fields'}
+        elif connection in self.connection:
+            response = {'success': False, 'message': f'Attempted to insert duplicate ID: {connection["_id"]}'}
+        else:
             self.connection.append(connection)
-            return True
-        return False
+            response = {'success': True, 'message': 'Success'}
+        return response
 
     def get_asset(self, asset_id):
         return item_by_asset_id(self.asset, asset_id)
 
-    def get_connection(self, asset_id):
-        return item_by_asset_id(self.connection, asset_id)
+    def get_connections(self, asset_id):
+        return items_by_asset_id(self.connection, asset_id)
+
+    def update_asset(self, asset_id, asset):
+        for i, item in enumerate(self.asset):
+            if item['asset_id'] == asset_id:
+                self.asset[i] = {**item, **asset}
+                return True
+        return False
+
+    def update_connection(self, _id, connection):
+        success = False
+        for i, item in enumerate(self.connection):
+            if item['_id'] == _id:
+                self.connection[i] = {**item, **connection}
+                success = True
+        return success
 
 
 def item_by_asset_id(items, asset_id):
     for item in items:
         if item['asset_id'] == asset_id:
             return item
-    return 'NOT FOUND'
+    return None
+
+
+def items_by_asset_id(items, asset_id):
+    results = []
+    for item in items:
+        if item['asset_id'] == asset_id:
+            results.append(item)
+    return results
